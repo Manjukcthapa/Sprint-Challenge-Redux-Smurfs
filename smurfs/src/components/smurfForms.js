5a100ff0271ccf816864b90a506baa37236ecb35
Manju KC <Manju KC>
2019-05-17 16:06:32 -0400

    changed some style

--- smurfs/src/components/smurfForms.js
+++ smurfs/src/components/smurfForms.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import "./smurfList.css"
 import { addSmurf } from '../actions';
 import { connect } from 'react-redux';
+
 
 class SmurfForm extends React.Component {
    constructor(props) {
@@ -37,14 +39,11 @@ class SmurfForm extends React.Component {
      render() {
        return (
 
-       <div className="smurfForm">
-         <div className='header-text'>
-             <h1>Join the Smurf Family</h1>
-         </div>
-           <form onSubmit={this.addSmurf}>
-           <div>
+       <div  className = "form">
+           <form  className onSubmit={this.addSmurf}>
+           <div className = "friendform">
            <input
-           className='name'
+           className='inputtype'
                onChange={this.handleInputChange}
                placeholder="name"
                value={this.state.name}
@@ -54,7 +53,7 @@ class SmurfForm extends React.Component {
            </div>
              <div>
              <input
-             className='age'
+             className='inputtype'
                onChange={this.handleInputChange}
                placeholder="age"
                value={this.state.age}
@@ -64,7 +63,7 @@ class SmurfForm extends React.Component {
              </div>
              <div>
              <input
-               className='height'
+               className='inputtype'
                onChange={this.handleInputChange}
                placeholder="height"
                value={this.state.height}
@@ -73,9 +72,9 @@ class SmurfForm extends React.Component {
              />
              </div>
 
-             <button className='button' type="submit">Add to the village</button>
+             <button className='button' type="submit">Add Friend</button>
            </form>
-       </div> // smurfForm end here //
+       </div> 
        );
    };
 }
@@ -143,91 +142,3 @@ export default connect(mapStateToProps, {addSmurf})(SmurfForm);
 
 
 
-// import  React from "reducer"
-
-// import {addSmurf} from "../action";
-// import {connect } from "react-redux"
-
-
-
-// class SmurfForm extends React.Component{
-//     construction (props){
-//         super(props);
-//         this.state = {
-//             name:"",
-//             age:"",
-//             height:""
-//         }
-//     }
-
-//     addSmurf = e => {
-//         e.preventDefault();
-//         const newSmurf = {
-//             name:this.state.name,
-//             age:thi.state.age,
-//             height:this.state.height
-//         }
-//     }
-
-//     this.props.addSmurf(newSmurf);
-
-//     this.setState({
-//         name:"",
-//         age:"",
-//         height:""
-//     })
-// }
-
-
-// handleInputChange = e => {
-//     this.setState({
-//         [e.target.name] : e.target.value
-//     })
-
-//     render(){
-//         return(
-//             <div>
-//                 <form onSubmit = {this.addSmurf}>
-//                 <input
-//                 onChange= {this.handleInputChange}
-//                 placeHolder="name"
-//                 value={this.state.name}
-//                 name="name"
-//                 type="text"
-//                 />
-
-// <input
-//                 onChange= {this.handleInputChange}
-//                 placeHolder="age"
-//                 value={this.state.age}
-//                 name="age"
-//                 type="number"
-//                 />
-
-// <input
-//                 onChange= {this.handleInputChange}
-//                 placeHolder="height"
-//                 value={this.state.height}
-//                 name="height"
-//                 type="text"
-//                 />
-
-//                 <button tpe="submit">Add Friends</button>
-//                 </form>
-//             </div>
-
-//         )
-//     }
-// }
-
-
-
-// const mapStateToProps = state => {
-//     return {
-//         addingSmurf: state.addingSmurf,
-//         error:state.error
-//     }
-// }
-
-
-// export default connect (mapStateToProps, {addSmurf}) (SmurfForm);
